e3353951f98129aaf7ca67445efeacce048225ff
BrandonCate95 <brandoncate95@gmail.com>
2026-06-07 21:04:39 -0700

    more concurrency tests, semaphores next

--- examples/simple/server.go
+++ examples/simple/server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
+	"math/rand/v2"
 	"net/http"
-	"os"
-
-	"github.com/agentstax/vulkan/pkg/workflow"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -15,20 +15,51 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
+// infinite loop ticker to feed data to queue
+func feedQueue(queue chan<- string) {
+	ticker := time.NewTicker(250 * time.Millisecond)
+
+	go func() {
+		for {
+			t := <-ticker.C
+			for i := range rand.IntN(10) {
+				queue <- t.String() + " " + strconv.Itoa(i)
+			}
+		}
+	}()
+}
+
 func main() {
 
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello", getHello)
+	// mux.HandleFunc("/hello", getHello)
 
-	mux.Handle("/scrape", workflow.ToHandler(&ScrapeWorkflow{}))
+	// mux.Handle("/scrape", workflow.ToHandler(&ScrapeWorkflow{}))
 
-	err := http.ListenAndServe(":3333", mux)
+	// err := http.ListenAndServe(":3333", mux)
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
+	// if errors.Is(err, http.ErrServerClosed) {
+	// 	fmt.Printf("server closed\n")
+	// } else if err != nil {
+	// 	fmt.Printf("error starting server: %s\n", err)
+	// 	os.Exit(1)
+	// }
+
+	const concurrencyLimit = 5
+	jobQueue := make(chan string, concurrencyLimit)
+
+	feedQueue(jobQueue)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		for {
+			timeString := <-jobQueue
+			fmt.Println(timeString)
+		}
+	}()
+
+	wg.Wait()
 }
